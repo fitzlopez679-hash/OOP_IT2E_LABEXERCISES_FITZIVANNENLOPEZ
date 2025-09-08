bc24987d42b67e08f2a9b00093e84e5d235b9006
fitzlopez679-hash <fitzlopez679@gmail.com>
2025-09-08 19:41:18 +0800

    Update Car.java

--- Exer2_Lopez/Car.java
+++ Exer2_Lopez/Car.java
@@ -1,1 +1,29 @@
+public class Car {
+    private String color;
+    private String licensePlate;
+    private String chassisNumber;
+    private String brandName;
+    
+    public Car() {
+        this.color = "Unknown";
+        this.licensePlate = "Unknown";
+        this.chassisNumber = "Unknown";
+        this.brandName = "Unknown";
+    }
 
+  
+    public Car(String color, String licensePlate, String chassisNumber, String brandName) {
+        this.color = color;
+        this.licensePlate = licensePlate;
+        this.chassisNumber = chassisNumber;
+        this.brandName = brandName;
+    }
+
+    public void displayInfo() {
+        System.out.println("Color: " + color);
+        System.out.println("License Plate: " + licensePlate);
+        System.out.println("Chassis Number: " + chassisNumber);
+        System.out.println("Brand Name: " + brandName);
+        System.out.println();
+    }
+}
